128c60e0540c92715ef091fae182414e6b48002c
itsdita <dita.varna@gmail.com>
2024-10-08 09:33:47 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/navbar/Navbar'
+import Blog from './components//blog/Blog'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Blog />
+    </>
+  )
+}
+
+export default App
