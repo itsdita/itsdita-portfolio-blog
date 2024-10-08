f9a5b394cccc59606c2045cc48850b0834217f52
itsdita <dita.varna@gmail.com>
2024-10-08 11:48:33 +0900

    Use styled components for ErrorPage styling
    To avoid creating new CSS file for ErrorPage styling, I used styled components.

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,21 +1,37 @@
 import { Link, useRouteError } from "react-router-dom";
 import errorImage from "../assets/404-error-page.svg";
+import styled from "styled-components";
 
 export default function ErrorPage() {
+  const ErrorContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 600px;
+    margin: 60px auto;
+    img {
+      width: 250px;
+      height: auto;
+      margin-bottom: 20px;
+    }
+    a {
+    margin-top: 20px;
+    text-decoration: none;
+    text-transform: uppercase;
+    color: blue;
+    }
+  `;
   const error = useRouteError();
   console.log(error);
   if (error.status === 404) {
     return (
-      <div style={{"margin-top": "30px"}}>
-        <img
-          src={errorImage}
-          alt="404 Error"
-          style={{ width: "250px", height: "250px" }}
-        />
+      <ErrorContainer>
+        <img src={errorImage} alt="404 Error" />
         <h2>Ohhh...</h2>
         <p>Page not found.</p>
         <Link to="/">Go back to the home page</Link>
-      </div>
+      </ErrorContainer>
     );
   }
 
