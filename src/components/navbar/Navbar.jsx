7dea2f4d97af217ab34cbc05af80e967aded0be1
itsdita <dita.varna@gmail.com>
2024-10-08 09:57:08 +0900

    Initial navbar setup, CSS cleanup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
-export default function Navbar() { 
-    return (
-        <nav>
-        <h1>Navbar</h1>
-        </nav>
-    )
-    }
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div id="nav-logo">
+        <a href="">itsDita</a>
+      </div>
+      <div id="nav-links">
+        <a href="">About</a>
+        <a href="">Portfolio</a>
+        <a href="">Blog</a>
+        <a href="">Notes</a>
+      </div>
+    </nav>
+  );
+}
